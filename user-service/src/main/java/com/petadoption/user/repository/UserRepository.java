bb94b14d17e495901ae8737cfdcb131a62f71b64
sompurajeel <sompurajeel12@gmail.com>
2026-09-01 12:20:48 +0530

    Delete user-service directory

--- user-service/src/main/java/com/petadoption/user/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.petadoption.user.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.petadoption.user.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
